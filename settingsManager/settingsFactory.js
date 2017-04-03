8815f615a5a0c7c4dbe30ec73bc760b70c78d04e
Grant Miller <blented@gmail.com>
2017-04-03 16:48:29 -0700

    add init to factory

--- settingsManager/settingsFactory.js
+++ settingsManager/settingsFactory.js
@@ -1,17 +1,23 @@
 var globalSettings = require('./globalSettings')
 var settingsManager = require('./settingsManager')
 
+var settingsManager = module.exports = {
 
-module.exports = {
+init: function(context, callback)
+{
+	settingsManager.database = context.database
+	context.settingsManager = settingsManager
+	callback()
+},
 
 globalSettings: function(paths, modes)
 {
 	return new globalSettings(paths, modes)
 },
 
-settingsManager: function(database, token, user)
+settingsManager: function(token, user)
 {
-	return new settingsManager(database, token, user)
+	return new settingsManager(settingsManager.database, token, user)
 }
 
 //end of module
